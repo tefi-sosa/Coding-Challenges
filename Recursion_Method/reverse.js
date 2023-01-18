4f4b7183eda57f718ae1c64bf6218a7faa91124d
Estefania Sosa <estefi.monumental@gmail.com>
2023-01-18 11:42:18 -0700

    Recursion

---
+++ Recursion_Method/reverse.js
@@ -0,0 +1,9 @@
+//  && Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+function reverse(str){
+	if(str.length <= 1) return str;
+	return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse('awesome')) // 'emosewa'
+console.log(reverse('rithmschool')) // 'loohcsmhtir'
